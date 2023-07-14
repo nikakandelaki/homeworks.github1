37f32cab8b39b678ff49ee0383146d07deb21b8a
nikakandelaki <139273739+nikakandelaki@users.noreply.github.com>
2023-07-14 21:48:51 +0400

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,52 @@
+#არასწორი პასუხები
+
+cel'sius = '25"
+
+f+ahrenheit = (celsius * 9/5) + 32
+
+pr\int("Temperature in Fahrenheit:, fa+hrenheit)
+
+
+Name = Nika
+age = '14;
+heigth = 1.71
+
+
+balance = 15' + 12
+print('balance is: ', balance)
+
+
+ja\mi = 10' - 2'
+pri/nt('subtotal is: ', jami)
+
+
+
+
+
+
+# სწორი პასუხები
+
+
+
+
+
+
+
+celsius = 25
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+
+Name = "Nika"
+age = 14
+heigth = 1.71
+
+
+balance = 15 + 12
+print('balance is: ', balance)
+
+
+jami = 10 - 2
+print('subtotal is: ', jami)
